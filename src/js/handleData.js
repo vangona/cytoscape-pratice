895a9e4aadb3e099f6f28154c4605390c91fc632
vangona <rlarhksrud12@naver.com>
2022-07-03 21:37:40 +0900

    Refactor: 코드 분리

---
+++ src/js/handleData.js
@@ -0,0 +1,30 @@
+const jsonData = require("../data/data.json");
+
+const getNodesFromData = (data) => {
+  return data;
+};
+
+const getEdgesFromData = (data) => {
+  let result = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (Object.keys(data[i]["data"]).includes("edgeTo")) {
+      result.push({
+        data: {
+          id: `${data[i]["data"]["id"]}/${data[i]["data"]["edgeTo"]}`,
+          target: `${data[i]["data"]["edgeTo"]}`,
+          source: `${data[i]["data"]["id"]}`,
+        },
+      });
+    }
+  }
+  return result;
+};
+
+const processDataFromJson = (rawData) => {
+  let nodes = getNodesFromData(rawData);
+  let edges = getEdgesFromData(rawData);
+  return [nodes, edges];
+};
+
+export const [processedNodes, processedEdges] = processDataFromJson(jsonData);
